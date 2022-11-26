740725d98a317a617d5b074c5496c242b24f6dda
tony555t <tonytullah764@gmail.com>
2022-11-26 15:06:10 +0300

    complete

---
+++ index.js
@@ -0,0 +1,21 @@
+//const divide = function(a, b) {
+  //  return a / b;
+  //};
+  
+  //console.log(divide(6, 2));
+
+  const divide = (parameter1, parameter2) => {
+    console.log(`divide ${parameter1}`);
+    console.log(`divide ${parameter2}`);
+    return 2000 / 100;
+  }
+  divide();
+  
+  const square = (a) => {
+    return a * a;
+}
+const add = (parameter1, parameter2) => {
+  console.log(`add ${parameter1}`);
+  console.log(`add ${parameter2}`);
+  return parameter1 + parameter2;
+}
